1d853442459ba5f4f1fa8f35a65c75881b4dd7c0
Nathan Cunningham <counterweight22@gmail.com>
2024-08-03 23:52:31 -0700

    Added both exchange rates. Changed specific calculations

--- STO.py
+++ STO.py
@@ -34,9 +34,10 @@ def BOB():
     auction_price = ws['B' + str(row)].value
     province = ws['C' + str(row)].value
     book_usd = ws['D' + str(row)].value
-    exchange = ws['K2'].value
+    cad_usd_exchange = ws['K2'].value
+    usd_cad_exchange = ws['L2'].value
 
-    book_cad = (2 - exchange) * book_usd
+    book_cad = usd_cad_exchange * book_usd
     ws['E' + str(row)].value = round(book_cad, 2)
 
 
@@ -46,28 +47,26 @@ def BOB():
       canadian_price = round(auction_price, 2)
       ws['F' + str(row)] = round(canadian_price, 2)
 
-      us_price = round(canadian_price * exchange, 2)                  # Converts the CAD to USD based on the input "Exchange rate"
+      us_price = round(canadian_price * cad_usd_exchange, 2)           # Converts the CAD to USD based on the input "Exchange rate"
       ws['G' + str(row)] = round(us_price, 2)
 
-      us_price += 2000                                                
-      us_price *= 1.01                                                # Adds 2000 and then adds 1% of the new total
+      us_price += 2500                                                # Adds 2000 and then adds 1% of the new total
       total_us_price = round(us_price, 2)  
       ws['H' + str(row)] = total_us_price                       
     elif province.lower() == 'ont':                                   # Checks if province is EAST
-      auction_price += 1000
+      auction_price += 800
       auction_price *= 1.13                                           # Adds 1000 and then adds 13% of the new total
       canadian_price = round(auction_price, 2)                        
       ws['F' + str(row)] = round(canadian_price, 2)
 
-      us_price = round(canadian_price * exchange, 2)                     # Converts the CAD to USD based on the input "Exchange Rate"
+      us_price = round(canadian_price * cad_usd_exchange, 2)           # Converts the CAD to USD based on the input "Exchange Rate"
       ws['G' + str(row)] = round(us_price, 2)
 
-      us_price += 1400                                                
-      us_price *= 1.01                                                # Adds 1400 and then adds 1% of the new total
+      us_price += 1400                                                # Adds 1400 and then adds 1% of the new total
       total_us_price = round(us_price, 2)
       ws['H' + str(row)] = total_us_price
 
-    bob = round(book_usd - total_us_price, 2)                             # Subtracts Total US from Book to calculate BOB
+    bob = round(book_usd - total_us_price, 2)                         # Subtracts Total US from Book to calculate BOB
     ws['I' + str(row)] = bob
 
 
